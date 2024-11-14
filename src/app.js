4e9de8e3e58e407888a239c0c84719c414cdd0ef
samarth2534@gmail.com <samarth2534@gmail.com>
2024-11-14 10:19:02 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -3,33 +3,17 @@ const express = require("express");
 const app = express();
 const { adminAuth } = require("./middlewares/auth");
 
-// app.use("/", (req, res, next) => {
-//   console.log("Handling / route");
-//   next();
-// });
+app.get("/getUserData", (req, res) => {
+  // Logic of db call and get user data
 
-// app.get("/user", (req, res, next) => {
-//   console.log("Handling the route user");
-//   next();
-// });
-
-// app.get("/user", (req, res, next) => {
-//   console.log("Handling the route user");
-//   res.send("Second route handler");
-// });
-
-// Importance of route handlers and middleware
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData", (req, res, next) => {
-  //logic of checking if the user is authorized
-  //Logic of fetching all data
-  res.send("All data sent");
+  throw new Error("random error");
+  res.send("User data sent");
 });
 
-app.delete("/admin/deleteUser", (req, res, next) => {
-  // checking if the user is authorized ==> instead of writing the logic everytime , we use middleware
-  res.send("delete a user");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen(7777, () =>
