3405040adb2df08dd7858bf67ac31a32aea294d3
samarth2534@gmail.com <samarth2534@gmail.com>
2024-11-18 18:33:23 +0530

    /getUser and /feed API done

--- src/app.js
+++ src/app.js
@@ -18,6 +18,43 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Get user by email
+app.get("/getUser", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something Went Wrong!!");
+  }
+
+  // try {
+  //   const users = await User.find({ emailId: userEmail });
+  //   if (users.length === 0) {
+  //     res.status(404).send("User not found");
+  //   } else {
+  //     res.send(users);
+  //   }
+  // } catch (err) {
+  //   res.status(400).send("Something Went Wrong!!");
+  // }
+});
+
+// FEED API -> get all the users from database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something Went Wrong!!");
+  }
+});
+
 connectDb()
   .then(() => {
     console.log("Database connection successful...");
