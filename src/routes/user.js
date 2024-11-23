5990e5c5c0ba3580206b3d92b17f2576a822a867
samarth2534@gmail.com <samarth2534@gmail.com>
2024-11-23 10:48:23 +0530

    Debugged an bug

--- src/routes/user.js
+++ src/routes/user.js
@@ -37,9 +37,16 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
           status: "accepted",
         },
       ],
-    }).populate("fromUserId", USER_SAFE_DATA);
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
-    const data = getAllConnectionsRequest.map((row) => row.fromUserId);
+    const data = getAllConnectionsRequest.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
 
     res.json({ data });
   } catch (err) {
