5dd7a934498afd43596cb79f9e96fd827347e110
samarth2534@gmail.com <samarth2534@gmail.com>
2025-02-01 16:21:00 +0530

    amazon ses code added

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -51,7 +51,11 @@ requestRouter.post(
       });
 
       const data = await connectionRequest.save();
-      
+
+      const emailRes = await sendEmail.run(
+        "A new friend request from " + req.user.firstName,
+        req.user.firstName + " is " + status + " in " + toUser.firstName
+      );
 
       res.json({
         message:
