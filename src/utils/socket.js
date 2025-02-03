1fa140711db95ed52b300cb1ffe47d10cf825a93
samarth2534@gmail.com <samarth2534@gmail.com>
2025-02-03 18:22:08 +0530

    Implemented real-tme chatting

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("_"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -10,19 +18,22 @@ const initializeSocket = (server) => {
   io.on("connection", (socket) => {
     // handle events
 
-    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
-      const a = String(userId);
-      const b = String(targetUserId);
-      const roomId = [a, b].sort();
-
+    socket.on("joinChat", (data) => {
+      const { firstName, userId, targetUserId } = data;
+      const trimmedTargetUserId = targetUserId.trim();
+      const roomId = getSecretRoomId(userId, trimmedTargetUserId);
       console.log(`${firstName} joined room: ${roomId}`);
       socket.join(roomId);
+     
     });
 
-    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = [userId, targetUserId].join("_");
-      console.log(firstName + " : " + text);
+    socket.on("sendMessage", (data) => {
+      
+      const { firstName, userId, targetUserId, text } = data;
+      const trimmedTargetUserId = targetUserId.trim();
+      const roomId = getSecretRoomId(userId, trimmedTargetUserId);
       io.to(roomId).emit("messageReceived", { firstName, text });
+      
     });
 
     socket.on("disconnect", () => {});
